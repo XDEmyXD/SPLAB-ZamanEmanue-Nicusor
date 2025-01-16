121ffdbfab6c89f0c2cdc242b6c42152697a361e
XDEmyXD <emanuel.zaman03@e-uvt.ro>
2025-01-17 01:56:39 +0200

    Initial commit

---
+++ src/main/java/com/example/splabzamanemanuenicusor/SplabZamanEmanueNicusorApplication.java
@@ -0,0 +1,13 @@
+package com.example.splabzamanemanuenicusor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SplabZamanEmanueNicusorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SplabZamanEmanueNicusorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/splabzamanemanuenicusor/SplabZamanEmanueNicusorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.splabzamanemanuenicusor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SplabZamanEmanueNicusorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
